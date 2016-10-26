2ebcac5123c32011c76fac40bbb712380aa3232a
Yuki3Ichihara <shandy_disarita@yahoo.co.jp>
2016-10-27 00:21:54 +0900

    Delete searchform.php

--- WordPress/sample_company/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" onsubmit= "if(this.s.value==''){return false}">
-	<input type="text" value="" name="s" id="s" class="entry">
-	<input id="searchsubmit" class="submit_button" type="submit" value="">
-</form>
