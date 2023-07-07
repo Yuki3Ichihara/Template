2eb482c8849eef5d03c743aa3f9e6bd0d8b88b38
Yuki3Ichihara <shandy_disarita@yahoo.co.jp>
2023-07-07 19:20:36 +0900

    Add files via upload

---
+++ Multi_Column/assets/js/drawer-menu.js
@@ -0,0 +1,48 @@
+$(function(){
+
+	$('.menu-button').on('click',function(){
+		$('.humbarger').toggleClass('humbarger-on');
+		$('.header-nav').toggleClass('header-nav-open');
+		$('.search-box').toggleClass('search-box-open');
+		$(".header-nav-open").css('opacity','0')
+		$(".header-nav-open").animate({'opacity': 1.0},500);
+		$('.category-detail-menu').toggleClass('mobile-category-detail-menu');
+		if($('.category-detail-menu').hasClass('mobile-category-detail-menu')){
+			$('.category-detail-menu').css({display:'none',marginLeft:'-80%'});
+		}
+		if($('.humbarger').hasClass('humbarger-on')){
+			$(".gray_layer").stop().fadeIn(500);
+		}else{
+			$(".gray_layer").stop().fadeOut(500);
+		}
+	});
+
+	$('.gray_layer').click(function(){
+		$('.humbarger').removeClass('humbarger-on');
+		$('.header-nav').removeClass('header-nav-open');
+		$('.search-box').removeClass('search-box-open');
+		$('.category-detail-menu').css({display:'none',marginLeft:'-80%'});
+		$('.category-detail-menu').removeClass('mobile-category-detail-menu');
+		if($('.category-detail-menu li').hasClass('return_btn')){
+			$('.return_btn').remove();
+		}
+		$(".gray_layer").fadeOut(500);
+	});
+
+	$(window).on('load resize', function(){
+		if(window.innerWidth >= 960){
+			if($('div').hasClass('humbarger-on')){
+				$('.humbarger').removeClass('humbarger-on');
+				$('.header-nav').removeClass('header-nav-open');
+				$('.search-box').removeClass('search-box-open');
+				$('.category-detail-menu').css({display:'none',marginLeft:'-80%'});
+				$('.category-detail-menu').removeClass('mobile-category-detail-menu');
+				if($('.category-detail-menu li').hasClass('return_btn')){
+					$('.return_btn').remove();
+				}
+				$(".gray_layer").fadeOut(500);
+			}
+		}
+	});
+
+});
